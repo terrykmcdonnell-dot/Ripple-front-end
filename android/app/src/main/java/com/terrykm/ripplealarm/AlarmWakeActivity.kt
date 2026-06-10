18bdcaae79e89e18ae72d864b58e07fcb6c113de
blackcatkai7040306 <blackcatkai7@gmail.com>
2026-06-10 01:21:51 -0400

    fix: fresh prebuild pinned AGP 8.3.2 gradle 8.6

--- android/app/src/main/java/com/terrykm/ripplealarm/AlarmWakeActivity.kt
+++ android/app/src/main/java/com/terrykm/ripplealarm/AlarmWakeActivity.kt
@@ -7,9 +7,9 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.net.Uri
 import android.view.Gravity
 import android.view.WindowManager
 import android.widget.LinearLayout
